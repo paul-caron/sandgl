80b5121019195cab92d0d818ba05b6efc8e0421d
paul <paul.a.caron@gmail.com>
2025-03-27 10:25:10 +0000

    update

--- src/events.js
+++ src/events.js
@@ -1,5 +1,20 @@
 function initEvents(){
+/*document.addEventListener('touchstart', function(e) {
+    e.preventDefault();
+}, { passive: false });
 
+document.addEventListener('touchmove', function(e) {
+    e.preventDefault();
+}, { passive: false });
+
+document.addEventListener('touchend', function(e) {
+    e.preventDefault();
+}, { passive: false });
+
+document.addEventListener('scroll', function(e) {
+    e.preventDefault();
+}, { passive: false });
+*/
 
     let form = document.querySelector("form");
     form.addEventListener("submit", (e)=>{
